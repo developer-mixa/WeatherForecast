ccddbda0ec33650e13ded103bc87440852ba4a93
developer-mixa <121621657+developer-mixa@users.noreply.github.com>
2024-06-04 10:11:07 +0300

    chore: update gradle configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,23 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+    versionCatalogs {
+        create("libs") {
+
+            library("android.coreKtx", "androidx.core:core-ktx:1.9.0")
+            library("android.appCompat", "androidx.appcompat:appcompat:1.6.1")
+            library("android.constraintLayout", "androidx.constraintlayout:constraintlayout:2.1.4")
+            library("android.fragmentKtx", "androidx.fragment:fragment-ktx:1.5.5")
+            library("android.activityKtx", "androidx.activity:activity-ktx:1.6.1")
+            library("android.lifecycleRuntimeKtx", "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+            library("android.lifecycleViewModelKtx", "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+            library("android.paging", "androidx.paging:paging-runtime-ktx:3.1.0")
+
+            library("google.material", "com.google.android.material:material:1.12.0")
+
+            library("test.junit", "junit:junit:4.13.2")
+        }
+    }
 }
 
 rootProject.name = "WeatherForecast"
